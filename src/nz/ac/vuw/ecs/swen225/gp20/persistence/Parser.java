b2fefde477dfeef7558aaaf0c25012e03196842b
Biru666 <68627484+Biru666@users.noreply.github.com>
2020-09-26 17:38:46 +1200

    Parser Adding

--- src/nz/ac/vuw/ecs/swen225/gp20/persistence/Parser.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/persistence/Parser.java
@@ -1,5 +1,42 @@
 package nz.ac.vuw.ecs.swen225.gp20.persistence;
 
-public class Parser {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import nz.ac.vuw.ecs.swen225.gp20.maze.Location;
+
+
+
+public class Parser {
+	JSONArray mapArray;
+	Map<Object, Location> map;
+	
+	public Parser(String filename) {
+		load(filename);
+	}
+	
+	private void load(String filename) {
+		JSONParser parser = new JSONParser();
+		try {
+			mapArray = (JSONArray) parser.parse(new FileReader(filename));
+			for(int i = 0; i < mapArray.size(); i++) {
+				JSONArray objectArray = (JSONArray) mapArray.get(i);
+				for(int j = 0; j < objectArray.size(); j++) {
+					
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
 }
