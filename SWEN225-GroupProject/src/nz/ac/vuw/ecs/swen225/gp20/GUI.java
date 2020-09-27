9b6a084d583bbc138d002db234b6080084ace4f0
phoenix <phoenix@10.140.89.99>
2020-09-27 21:28:48 +1300

    GUI added

---
+++ SWEN225-GroupProject/src/nz/ac/vuw/ecs/swen225/gp20/GUI.java
@@ -0,0 +1,48 @@
+package nz.ac.vuw.ecs.swen225.gp20;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import javax.swing.JFrame;
+
+/**
+ * GUI for the whole game.
+ * 
+ * @author Phoenix Xie
+ *
+ */
+public class GUI extends JFrame implements ComponentListener {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void componentResized(ComponentEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void componentMoved(ComponentEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void componentShown(ComponentEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void componentHidden(ComponentEvent e) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+
+}
