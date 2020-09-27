617381c9fbdf5cfebbe6cc0847044bab7ec4d406
Dingye <12036dw@gmail.com>
2020-09-27 15:31:22 +1300

    action enum comment

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Action.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Action.java
@@ -1,5 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
+/**
+ * Action enums
+ * 
+ * @author wangding1 300422014
+ *
+ */
 public enum Action {
 	MOVE, WALL, DOOR, ITEM, INFO, EXIT, DIE, INVALID
 }
