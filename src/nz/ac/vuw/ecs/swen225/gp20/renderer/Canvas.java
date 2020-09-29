3d20a8c55dda8d45c3982ec3a26ffe7af7f3b525
phoenix <phoenix@10.140.89.99>
2020-09-30 05:09:07 +1300

    Add icons image and delete duplicate GUI

--- src/nz/ac/vuw/ecs/swen225/gp20/renderer/Canvas.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/renderer/Canvas.java
@@ -1,8 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp20.renderer;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import nz.ac.vuw.ecs.swen225.gp20.application.*;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
 
 
 /**
@@ -15,16 +18,20 @@ public class Canvas extends JPanel {
    * 
    */
   private static final long serialVersionUID = 1L;
-
+  
+  public static final int VIEW_SIZE = 9;
+  private Main GUI = new Main();
+  private JLabel[][] displayTiles = new JLabel[VIEW_SIZE][VIEW_SIZE];
+  private Tile[][] gameTiles = null;
+	
+  private double width = 500; 
+  private double height = 500;
 
   /**
    * 
    */
   public Canvas() {
-    setPreferredSize(new Dimension(GUI.CANVAS_SIZE, GUI.CANVAS_SIZE));
-
     
-    setBackground(Color.red);
   }
 
 }
