1f4bbb0b11f824c77bc24411f89a91f1940e70d5
Yaxin Liang <liangyaxi@ecs.vuw.ac.nz>
2020-10-01 01:18:13 +0800

    Build the game state

--- src/nz/ac/vuw/ecs/swen225/gp20/recnplay/RecordAndReplay.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/recnplay/RecordAndReplay.java
@@ -19,6 +19,18 @@ import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
 
+import nz.ac.vuw.ecs.swen225.gp20.persistence.Parser;
+import nz.ac.vuw.ecs.swen225.gp20.application.GameController;
+import nz.ac.vuw.ecs.swen225.gp20.maze.ActorName;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Door;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Location;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Key;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
+import nz.ac.vuw.ecs.swen225.gp20.maze.TileName;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Variation;
+
+//import nz.ac.vuw.ecs.swen225.gp20.maze.Direction;
+
 
 /**
  * Class to record game play and replay it.
@@ -44,6 +56,12 @@ public class RecordAndReplay {
 	
 	public static void saveRecording() {
 		if(isRecording) {
+			String gameState = null;
+			/** if using the saveGame in persistence package
+			 * SaveGame s = new SaveGame(maze, currentState);
+			 * gameState = s.save();
+			*/
+			//gameState = getGameState();
 			JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
 			for (int i = 0; i < actions.size(); ++i) {
 				KeyEvent e = actions.get(i);
@@ -69,7 +87,8 @@ public class RecordAndReplay {
 		        arrayBuilder.add(builder.build());
 		      }
 			JsonObjectBuilder builder = Json.createObjectBuilder()
-			          .add("moves", arrayBuilder);
+					.add("game", gameState)
+			        .add("moves", arrayBuilder);
 			
 			Writer writer = new StringWriter();
 			Json.createWriter(writer).write(builder.build());
@@ -85,13 +104,127 @@ public class RecordAndReplay {
 		}
 	}
 	
+	
 	public void loadRecording() {
 		
+	}
+	
+	private static String getGameState() {
+		String gamestate = null;
+		Location[][]map = null;
+		Parser parser = new Parser(GameController.class.getResource("/level1.json").getPath());
+		map = parser.map;
+		for(int i=0; i<map.length; i++) {
+			for(int j=0; j<map[0].length; j++) {
+				String gs = null;
+				Location object = map[i][j];
+				TileName tileName = object.getTile().getTileName();
+				ActorName actorName = object.getActor().getActorName();
+				switch(actorName) {
+				case CHAP:
+					gs = "1";
+					gamestate = add(gamestate,gs);
+					break;
+				case BOT:
+					gs = "15";
+					gamestate = add(gamestate,gs);
+				}
+				switch (tileName) {
+				// wall tile
+				case WALL:
+					gs = "2";
+					gamestate = add(gamestate,gs);
+					break;
+				// chips
+				case CHIP:
+					gs = "3";
+					gamestate = add(gamestate,gs);
+					break;
+				// Key
+				case KEY:
+					Key k = (Key) object.getTile();
+					Variation v = k.getVariation();
+					switch(v) {
+					case YELLOW:
+						gs = "4";
+						gamestate = add(gamestate,gs);
+						break;
+					case RED:
+						gs = "5";
+						gamestate = add(gamestate,gs);
+						break;
+					case GREEN:
+						gs = "6";
+						gamestate = add(gamestate,gs);
+						break;
+					case BLUE:
+						gs = "7";
+						gamestate = add(gamestate,gs);
+						break;
+					}
+				// Door
+				case DOOR:
+					Door d = (Door) object.getTile();
+					Variation v2 = d.getVariation();
+					switch(v2) {
+					case YELLOW:
+						gs = "8";
+						gamestate = add(gamestate,gs);
+						break;
+					case RED:
+						gs = "9";
+						gamestate = add(gamestate,gs);
+						break;
+					case GREEN:
+						gs = "10";
+						gamestate = add(gamestate,gs);
+						break;
+					case BLUE:
+						gs = "11";
+						gamestate = add(gamestate,gs);
+						break;
+					}
+				// Gate
+				case GATE:
+					gs = "12";
+					gamestate = add(gamestate,gs);
+					break;
+				// Portal
+				case EXIT:
+					gs = "13";
+					gamestate = add(gamestate,gs);
+					break;
+				// Help
+				case INFO:
+					gs = "14";
+					gamestate = add(gamestate,gs);
+					break;
+				// Sand
+				default:
+					gs = "34";
+					gamestate = add(gamestate,gs);
+					break;
+				}
+			}
+		}
+		return gamestate;
+	}
+	
+	private static String add(String s1, String s2) {
+		return s1+s2;
 	}
 	
 	public static boolean isRecording() {
 		if(isRecording) return true;
 		return false;
+	}
+	
+	public ArrayList<KeyEvent> getActions(){
+		return actions;
+	}
+	
+	public String getFileName() {
+		return fileName;
 	}
 	
 }
