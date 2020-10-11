b3d92f9b4fd0e4c39a126919fcd843cce731c6f5
phoenix <phoenix@10.140.98.206>
2020-10-11 18:31:00 +1300

    could render the chap now, but has problem after moving,nee d to be fix.

--- src/nz/ac/vuw/ecs/swen225/gp20/renderer/Canvas.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/renderer/Canvas.java
@@ -10,6 +10,9 @@ import java.awt.Image;
 import java.net.URL;
 
 import nz.ac.vuw.ecs.swen225.gp20.application.*;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Actor;
+import nz.ac.vuw.ecs.swen225.gp20.maze.ActorName;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Location;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
 
 
@@ -27,7 +30,9 @@ public class Canvas extends JPanel {
   public static final int VIEW_SIZE = 9;
   
   private JLabel[][] displayTiles = new JLabel[VIEW_SIZE][VIEW_SIZE];
-  private Tile[][] gameTiles = null;
+  private Location[][] map = null;
+  //private Location chapL=null;
+  
   private double width = 500; 
   private double height = 500;
 
@@ -51,32 +56,57 @@ public class Canvas extends JPanel {
   }
   
   public void paintTiles() {
-		if(gameTiles == null || width == 0 || height == 0) { return; }
+		if(map == null || width == 0 || height == 0) { return; }
 		for(int i = 0; i < VIEW_SIZE; i++) {
 			for(int j = 0; j < VIEW_SIZE; j++) {
-				displayTiles[i][j].setIcon(new ImageIcon(getImage(gameTiles[i][j])));
+
+				displayTiles[i][j].setIcon(new ImageIcon(getImage(map[i][j])));
 			}
 		}
+		//displayTiles[5][5].setIcon(new ImageIcon(getChapImage()));
 	}
+  
+  
 
-  private Image getImage(Tile name) {
-	  if (name == null || name.getTileName() == null) {
+  private Image getImage(Location l) {
+	  if(l.getActor() != null && l.getActor().getActorName()==ActorName.CHAP) {
+			URL url = getClass().getResource("/CHAP.png");
+		    Image icon = new ImageIcon(url).getImage();
+		    
+		    return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+			  }
+ if (l == null || l.getTile() == null && l.getActor()==null) {
 		  return new ImageIcon("/floor.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
 	  }
-	String tilename = name.getTileName().toString();
+//	  if(l.getTile() != null&&l.getActor()==null) {
+	String name = l.getTile().getTileName().toString();
+	
 	//TODO  needs to be fix for color choose
+	//String color = l.getTile().getVariation().toString();
 	//String color = name.getTileName() ;
-	String s = tilename+".png";
+	String s = name+".png";
 	
 	URL url = getClass().getResource("/" + s);
     Image icon = new ImageIcon(url).getImage();
     
     return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-	
+//	  }
+	  
+//delete the prouv one if work
+//	  return new ImageIcon("/floor.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
 }
+  
+  private Image getChapImage() {
+	  //String name = chap.getActorName().toString();
+	  String s = "CHAP.png";
+	  URL url = getClass().getResource("/" + s);
+	  Image icon = new ImageIcon(url).getImage();
+	  
+	return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+  }
 
-	public void setGameTiles(Tile[][] tiles) {
-	this.gameTiles = tiles;
+	public void setGameTiles(Location[][] tiles) {
+	this.map = tiles;
 	paintTiles();
 //	revalidate();
 }
