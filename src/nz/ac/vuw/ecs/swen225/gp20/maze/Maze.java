76df5b203a000f39cdeffb1b670ea42adb4e455a
Dingye <12036dw@gmail.com>
2020-10-21 11:37:58 +1300

    Added precondition guava lib with some precon and assert checks

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Maze.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Maze.java
@@ -1,5 +1,9 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.base.Preconditions;
+
 /**
  * Maze class which stores the current level information of the game
  * 
@@ -18,6 +22,7 @@ public class Maze {
 	 * @param locations - 2d Array of Locations (x,y)
 	 */
 	public void setLevel(Location[][] locations) {
+		Preconditions.checkArgument(locations != null, "2D Location array cannot be null"); // Precondition check
 		this.locations = locations;
 		this.chap = findChap(locations);
 	}
@@ -38,6 +43,7 @@ public class Maze {
 	 * @param direction - Direction enum
 	 */
 	public void move(Actor actor, Direction direction) {
+		Preconditions.checkArgument(actor != null, "The specified Actor object does not exist");
 		int currentRow = actor.getLocation().getRow();
 		int currentCol = actor.getLocation().getCol();
 		action = Action.INVALID;
@@ -46,20 +52,22 @@ public class Maze {
 		switch (direction) {
 		case NORTH:
 			if ((newL = getValidLocation(currentCol, currentRow - 1)) != null)
-				action = actor.move(newL,direction);
+				action = actor.move(newL, direction);
 			break;
 		case SOUTH:
 			if ((newL = getValidLocation(currentCol, currentRow + 1)) != null)
-				action = actor.move(newL,direction);
+				action = actor.move(newL, direction);
 			break;
 		case EAST:
 			if ((newL = getValidLocation(currentCol + 1, currentRow)) != null)
-				action = actor.move(newL,direction);
+				action = actor.move(newL, direction);
 			break;
 		case WEST:
 			if ((newL = getValidLocation(currentCol - 1, currentRow)) != null)
-				action = actor.move(newL,direction);
+				action = actor.move(newL, direction);
 			break;
+		default:
+			assertTrue("Direction enum is not a valid direction", true);
 		}
 	}
 
@@ -74,6 +82,7 @@ public class Maze {
 		Location newL = null;
 		if ((newX >= 0 && newX <= locations[0].length) && (newY >= 0 && newY <= locations.length))
 			newL = locations[newX][newY];
+		assertTrue("The new Location is currently out of bounds", newL != null);
 		return newL;
 	}
 
@@ -93,15 +102,19 @@ public class Maze {
 				Tile t = loc.getTile();
 
 				// if actor is chap, set chap
-				if (a != null && a.getActorName() == ActorName.CHAP)
+				if (a != null && a.getActorName() == ActorName.CHAP) {
+					assertTrue("Multiple Chaps has been detected in the current level.", c == null);
 					c = (Chap) a;
+				}
 
+				// counting total chips
 				if (t != null && t.getTileName() == TileName.CHIP)
 					totChips++;
 			}
 		}
 		if (c != null)
 			c.setTotalChips(totChips);
+		assertTrue("No Chap has been detected in the current level.", c != null);
 		return c;
 	}
 
@@ -111,12 +124,13 @@ public class Maze {
 	 * @return chap - Chap object
 	 */
 	public Chap getChap() {
+		Preconditions.checkArgument(chap != null, "Chap dose not currently exsist");
 		return chap;
 	}
 
 	/**
 	 * Returns the action after a move
-	 * 
+	 *   
 	 * @return Action enum
 	 */
 	public Action getAction() {
@@ -129,6 +143,7 @@ public class Maze {
 	 * @return locations - 2d Array of Location objects
 	 */
 	public Location[][] getLocation() {
+		Preconditions.checkArgument(locations != null, "2d array Locations dose not currently exsist");
 		return locations;
 	}
 
