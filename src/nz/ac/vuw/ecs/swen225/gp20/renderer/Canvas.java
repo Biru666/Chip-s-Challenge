3a02b166d1c00c9c2d2b760e7966ec9bef454cce
phoenix <phoenix@10.140.98.206>
2020-10-21 23:57:12 +1300

    level 2 Bug image render fixed

--- src/nz/ac/vuw/ecs/swen225/gp20/renderer/Canvas.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/renderer/Canvas.java
@@ -1,16 +1,13 @@
 package nz.ac.vuw.ecs.swen225.gp20.renderer;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.net.URL;
 
-import nz.ac.vuw.ecs.swen225.gp20.application.*;
-import nz.ac.vuw.ecs.swen225.gp20.maze.Actor;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import nz.ac.vuw.ecs.swen225.gp20.maze.ActorName;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Door;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Key;
@@ -18,136 +15,126 @@ import nz.ac.vuw.ecs.swen225.gp20.maze.Location;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Variation;
 
-
 /**
  * @author phoenix
  *
  */
 public class Canvas extends JPanel {
 
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 1L;
-  
-  public static final int VIEW_SIZE = 9;
-  
-  private JLabel[][] displayTiles = new JLabel[VIEW_SIZE][VIEW_SIZE];
-  private Location[][] map = null;
-  //private Location chapL=null;
-  
-  private double width = 500; 
-  private double height = 500;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-  /**
-   * 
-   */
-  public Canvas() {
-	  this.setLayout(new GridLayout(9, 9, 2, 2));
-	  
-	  //Initialize the tile name to be display
-	  for(int i = 0; i < VIEW_SIZE; i++) {
-			for(int j = 0; j < VIEW_SIZE; j++) {				
+	public static final int VIEW_SIZE = 9;
+
+	private JLabel[][] displayTiles = new JLabel[VIEW_SIZE][VIEW_SIZE];
+	private Location[][] map = null;
+	// private Location chapL=null;
+
+	private double width = 500;
+	private double height = 500;
+
+	/**
+	 * 
+	 */
+	public Canvas() {
+		this.setLayout(new GridLayout(9, 9, 2, 2));
+
+		// Initialize the tile name to be display
+		for (int i = 0; i < VIEW_SIZE; i++) {
+			for (int j = 0; j < VIEW_SIZE; j++) {
 				displayTiles[i][j] = new JLabel();
 				this.add(displayTiles[i][j]);
 			}
 		}
-	  
-	
-	  this.setVisible(true);
-    
-  }
-  
-  public void paintTiles() {
-		if(map == null || width == 0 || height == 0) { return; }
-		for(int i = 0; i < VIEW_SIZE; i++) {
-			for(int j = 0; j < VIEW_SIZE; j++) {
+
+		this.setVisible(true);
+
+	}
+
+	public void paintTiles() {
+		if (map == null || width == 0 || height == 0) {
+			return;
+		}
+		for (int i = 0; i < VIEW_SIZE; i++) {
+			for (int j = 0; j < VIEW_SIZE; j++) {
 
 				displayTiles[i][j].setIcon(new ImageIcon(getImage(map[i][j])));
 			}
 		}
-		//displayTiles[5][5].setIcon(new ImageIcon(getChapImage()));
 	}
-  
-  
 
-  private Image getImage(Location l) {
-	  if(l.getActor() != null && l.getActor().getActorName()==ActorName.CHAP) {
-			URL url = getClass().getResource("/CHAP.png");
-		    Image icon = new ImageIcon(url).getImage();
-		    
-		    return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-			  }
- if (l == null || l.getTile() == null && l.getActor()==null) {
-		  return new ImageIcon(getClass().getResource("/floor.png")).getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-	  }
- Tile tile = l.getTile();
- if (tile instanceof Key) {
-		Key key = (Key) tile;
-		Variation variation = key.getVariation();
-		if (variation == Variation.BLUE) {
-			return new ImageIcon(getClass().getResource("/blue_key.png")).getImage().getScaledInstance(50, 50,
-					Image.SCALE_DEFAULT);
-		} else if (variation == Variation.YELLOW) {
-			return new ImageIcon(getClass().getResource("/yellow_key.png")).getImage().getScaledInstance(50, 50,
-					Image.SCALE_DEFAULT);
-		} else if (variation == Variation.GREEN) {
-			return new ImageIcon(getClass().getResource("/green_key.png")).getImage().getScaledInstance(50, 50,
-					Image.SCALE_DEFAULT);
-		} else if (variation == Variation.RED) {
-			return new ImageIcon(getClass().getResource("/red_key.png")).getImage().getScaledInstance(50, 50,
+	private Image getImage(Location l) {
+		if (l.getActor() != null) {
+			if (l.getActor().getActorName() == ActorName.CHAP) {
+				URL url = getClass().getResource("/CHAP.png");
+				Image icon = new ImageIcon(url).getImage();
+
+				return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+			} else if (l.getActor().getActorName() == ActorName.BOT) {
+				URL url = getClass().getResource("/bug.png");
+				Image icon = new ImageIcon(url).getImage();
+
+				return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+			}
+		}
+		if (l == null || l.getTile() == null && l.getActor() == null) {
+			return new ImageIcon(getClass().getResource("/floor.png")).getImage().getScaledInstance(50, 50,
 					Image.SCALE_DEFAULT);
 		}
-	}
-	if (tile instanceof Door) {
-		Door door = (Door) tile;
-		Variation variation = door.getVariation();
-		if (variation == Variation.BLUE) {
-			return new ImageIcon(getClass().getResource("/blue_door.png")).getImage().getScaledInstance(50, 50,
-					Image.SCALE_DEFAULT);
-		} else if (variation == Variation.YELLOW) {
-			return new ImageIcon(getClass().getResource("/yellow_door.png")).getImage().getScaledInstance(50, 50,
-					Image.SCALE_DEFAULT);
-		} else if (variation == Variation.GREEN) {
-			return new ImageIcon(getClass().getResource("/green_door.png")).getImage().getScaledInstance(50, 50,
-					Image.SCALE_DEFAULT);
-		} else if (variation == Variation.RED) {
-			return new ImageIcon(getClass().getResource("/red_door.png")).getImage().getScaledInstance(50, 50,
-					Image.SCALE_DEFAULT);
+		Tile tile = l.getTile();
+		if (tile instanceof Key) {
+			Key key = (Key) tile;
+			Variation variation = key.getVariation();
+			if (variation == Variation.BLUE) {
+				return new ImageIcon(getClass().getResource("/blue_key.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			} else if (variation == Variation.YELLOW) {
+				return new ImageIcon(getClass().getResource("/yellow_key.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			} else if (variation == Variation.GREEN) {
+				return new ImageIcon(getClass().getResource("/green_key.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			} else if (variation == Variation.RED) {
+				return new ImageIcon(getClass().getResource("/red_key.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			}
 		}
+		if (tile instanceof Door) {
+			Door door = (Door) tile;
+			Variation variation = door.getVariation();
+			if (variation == Variation.BLUE) {
+				return new ImageIcon(getClass().getResource("/blue_door.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			} else if (variation == Variation.YELLOW) {
+				return new ImageIcon(getClass().getResource("/yellow_door.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			} else if (variation == Variation.GREEN) {
+				return new ImageIcon(getClass().getResource("/green_door.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			} else if (variation == Variation.RED) {
+				return new ImageIcon(getClass().getResource("/red_door.png")).getImage().getScaledInstance(50, 50,
+						Image.SCALE_DEFAULT);
+			}
+		}
+
+		String name = l.getTile().getTileName().toString();
+
+		// TODO needs to be fix for color choose
+		// String color = l.getTile().getVariation().toString();
+		// String color = name.getTileName() ;
+		String s = name + ".png";
+
+		URL url = getClass().getResource("/" + s);
+		Image icon = new ImageIcon(url).getImage();
+
+		return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
 	}
- 
-//	  if(l.getTile() != null&&l.getActor()==null) {
-	String name = l.getTile().getTileName().toString();
-	
-	//TODO  needs to be fix for color choose
-	//String color = l.getTile().getVariation().toString();
-	//String color = name.getTileName() ;
-	String s = name+".png";
-	
-	URL url = getClass().getResource("/" + s);
-    Image icon = new ImageIcon(url).getImage();
-    
-    return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-//	  }
-	  
-//delete the prouv one if work
-//	  return new ImageIcon("/floor.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-}
-  
-  private Image getChapImage() {
-	  //String name = chap.getActorName().toString();
-	  String s = "CHAP.png";
-	  URL url = getClass().getResource("/" + s);
-	  Image icon = new ImageIcon(url).getImage();
-	  
-	return icon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-  }
 
 	public void setGameTiles(Location[][] tiles) {
-	this.map = tiles;
-	paintTiles();
-//	revalidate();
-}
+		this.map = tiles;
+		paintTiles();
+	}
 
 }
